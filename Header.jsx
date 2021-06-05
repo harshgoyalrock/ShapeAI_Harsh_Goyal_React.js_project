f9814495a61bb62bb21ab58631b7172ad1f98b81
harshgoyalrock <75447377+harshgoyalrock@users.noreply.github.com>
2021-06-05 21:49:47 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+  
+import React  from "react";
+
+function Header() {
+  return  ( 
+    <header>
+      <h1 style={{color: "rgb(255, 0, 0)"}}>ShapeAI</h1>
+    </header>
+  );
+}
+
+export default Header;
